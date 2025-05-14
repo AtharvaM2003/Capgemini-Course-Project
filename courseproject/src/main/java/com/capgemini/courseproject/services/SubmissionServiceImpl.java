96a120a755316873673079c0b439d129c6b4f729
Vedant2510-k <vkarhale25@gmail.com>
2025-05-14 15:42:43 +0530

    submission commit by vedant

---
+++ courseproject/src/main/java/com/capgemini/courseproject/services/SubmissionServiceImpl.java
@@ -0,0 +1,41 @@
+package com.capgemini.courseproject.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.capgemini.courseproject.entities.Submission;
+import com.capgemini.courseproject.repositories.SubmissionRepository;
+
+@Service
+public class SubmissionServiceImpl implements SubmissionService {
+	
+	private final SubmissionRepository repository;
+
+	@Autowired
+	public SubmissionServiceImpl(SubmissionRepository repository) {
+		this.repository = repository;
+	}
+
+	@Override
+	public List<Submission> findAllsubmissions() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Submission findSubmissionById(Long submissionId) {
+		return repository.findById(submissionId)
+				.orElseThrow(() -> new SubmissionNotFoundException("Submission not found with ID: " + submissionId));
+	}
+
+	@Override
+	public boolean deleteSubmission(Long submissionId) {
+		if (repository.existsById(submissionId)) {
+			repository.deleteById(submissionId);
+			return true;
+		}
+		return false;
+	}
+
+}
