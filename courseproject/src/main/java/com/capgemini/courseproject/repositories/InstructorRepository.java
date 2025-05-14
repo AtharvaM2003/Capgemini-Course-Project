6add7c3099c91314a647e19975deefef99961bad
Suraj Salunkhe <surajsalunkhe1323@gmail.com>
2025-05-14 15:41:23 +0530

    Instructor Repository

---
+++ courseproject/src/main/java/com/capgemini/courseproject/repositories/InstructorRepository.java
@@ -0,0 +1,9 @@
+package com.capgemini.courseproject.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.capgemini.courseproject.entities.Instructor;
+
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+
+}
