a2e22b7e7be8949b9430e3ba0df8b71518d492a6
Aarya <21512320.dypit@dypvp.edu.in>
2025-05-14 14:48:11 +0530

    All packages created

--- courseproject/src/main/java/com/capgemini/courseproject/entities/User.java
+++ courseproject/src/main/java/com/capgemini/courseproject/entities/User.java
@@ -1,5 +1,111 @@
 package com.capgemini.courseproject.entities;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+
+	private String name;
+
+	private String email;
+
+	private String password;
+
+	private String phone;
+
+	private String userType;
+
+//    @OneToMany(mappedBy = "user")
+//    private List<Enrollment> enrollments;
+
+//    @OneToMany(mappedBy = "user")
+//    private List<Submission> submissions;
+
+	public User() {
+		super();
+	}
+
+	public User(Long userId, String name, String email, String password, String phone, String userType,
+			List<Enrollment> enrollments, List<Submission> submissions) {
+		super();
+		this.userId = userId;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.phone = phone;
+		this.userType = userType;
+//		this.enrollments = enrollments;
+//		this.submissions = submissions;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getUserType() {
+		return userType;
+	}
+
+	public void setUserType(String userType) {
+		this.userType = userType;
+	}
+
+//	public List<Enrollment> getEnrollments() {
+//		return enrollments;
+//	}
+//
+//	public void setEnrollments(List<Enrollment> enrollments) {
+//		this.enrollments = enrollments;
+//	}
+//
+//	public List<Submission> getSubmissions() {
+//		return submissions;
+//	}
+//
+//	public void setSubmissions(List<Submission> submissions) {
+//		this.submissions = submissions;
+//	}
 
 }
