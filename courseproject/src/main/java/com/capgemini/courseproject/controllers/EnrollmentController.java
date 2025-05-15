74a3e3a9f876bbdc22090c5deaadd782c6ed91fa
Vedang <vedangghui913@gmail.com>
2025-05-15 16:02:56 +0530

    Binding result in enrollment

--- courseproject/src/main/java/com/capgemini/courseproject/controllers/EnrollmentController.java
+++ courseproject/src/main/java/com/capgemini/courseproject/controllers/EnrollmentController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capgemini.courseproject.entities.Enrollment;
 import com.capgemini.courseproject.services.EnrollmentService;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
@@ -33,7 +35,12 @@ public class EnrollmentController {
 
 
 	@PostMapping
-	public ResponseEntity<Enrollment> createEnrollment(@RequestBody Enrollment enrollment) {
+	public ResponseEntity<Enrollment> createEnrollment(@Valid @RequestBody Enrollment enrollment, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			
+			throw new IllegalArgumentException(result.getFieldErrors().toString());
+		}
 		
 		log.info("Request to Create a new enrollment");
 
