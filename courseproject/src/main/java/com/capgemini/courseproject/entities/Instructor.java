0acc9792fda53bb636a1d5557980020eada2d0a2
Suraj Salunkhe <surajsalunkhe1323@gmail.com>
2025-05-15 15:29:40 +0530

    Instructor Validation And Logging

--- courseproject/src/main/java/com/capgemini/courseproject/entities/Instructor.java
+++ courseproject/src/main/java/com/capgemini/courseproject/entities/Instructor.java
@@ -1,7 +1,7 @@
 package com.capgemini.courseproject.entities;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-
 
 import java.util.ArrayList;
 
@@ -13,22 +13,25 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 @Entity
 @Table(name = "instructors")
 public class Instructor {
-	  @Id
-	    @GeneratedValue(strategy = GenerationType.IDENTITY)
-	    @Column(name = "instructor_id")
-	    private Long instructorId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "instructor_id")
+	private Long instructorId;
 
-	    @NotBlank
-	    @Column(name = "name")
-	    private String name;
+	@NotBlank(message = "Instructor name must not be blank")
+	@Size(min = 2, max = 100, message = "Instructor name must be between 2 and 100 characters")
+	@Column(name = "name")
+	private String name;
 
-	    @NotBlank
-	    @Column(name = "expertise")
-	    private String expertise;
+	@NotBlank(message = "Expertise must not be blank")
+	@Size(min = 2, max = 100, message = "Expertise must be between 2 and 100 characters")
+	@Column(name = "expertise")
+	private String expertise;
 
-	    @OneToMany(mappedBy = "instructor", cascade = CascadeType.ALL, orphanRemoval = true)
-	    @JsonManagedReference(value = "instructor_course")
-	    private List<Course> courses = new ArrayList<>();
+	@OneToMany(mappedBy = "instructor", cascade = CascadeType.ALL, orphanRemoval = true)
+	@JsonManagedReference(value = "instructor_course")
+	private List<Course> courses = new ArrayList<>();
+
 	public Instructor() {
 		super();
 	}
