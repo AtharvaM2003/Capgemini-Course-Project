1214da79ae1751fb8a9d505caf4bb78982a7111e
SAHIL-4432 <sahil.shelke4432@gmail.com>
2025-05-17 16:42:10 +0530

    Dashboard Commit

--- courseproject/src/main/java/com/capgemini/courseproject/services/InstructorService.java
+++ courseproject/src/main/java/com/capgemini/courseproject/services/InstructorService.java
@@ -2,9 +2,14 @@ package com.capgemini.courseproject.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.capgemini.courseproject.dto.InstructorWiseStudentDto;
 import com.capgemini.courseproject.entities.Instructor;
+import com.capgemini.courseproject.repositories.InstructorRepository;
 
 public interface InstructorService {
+
 	List<Instructor> findAllInstructor();
 
 	Instructor findInstructorById(Long instructorId);
@@ -14,5 +19,6 @@ public interface InstructorService {
 	Instructor updateInstructor(Instructor instructor, Long instructorId);
 
 	boolean deleteInstructor(Long instructorId);
-
+	
+	List<InstructorWiseStudentDto> getInstructorWiseStudentCount();
 }
